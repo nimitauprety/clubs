d90e1a44ca58d0579b413ff39f09c586bec86173
Nimita Uprety <uprety.nimita@gmail.com>
2021-04-29 00:47:40 -0500

    third committ

--- app/src/main/java/com/ubs/clubs/CreateClubActivity.java
+++ app/src/main/java/com/ubs/clubs/CreateClubActivity.java
@@ -2,7 +2,10 @@ package com.ubs.clubs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CreateClubActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class CreateClubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_club);
+
+        EditText NewClubName = (EditText) findViewById(R.id.NewClubName);
+        String clubname = NewClubName.getText().toString();
+
+        EditText NewClubDescription = (EditText) findViewById(R.id.NewClubDescription);
+        String clubdescription = NewClubDescription.getText().toString();
+
+//        submitcreateclub_button =findViewById(R.id.submitcreateclub_button)
+//
+//        submitcreateclub_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent (CreateClubActivity.this,JoinClubActivity.class);
+//                startActivity(intent);
+//            }
+//
+//        });
+
     }
 }
